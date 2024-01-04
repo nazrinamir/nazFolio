33699307ba44c711dd0787baf46932691341938e
Nazrin Amirrullah bin Zaidi <147366697+nazrinamir@users.noreply.github.com>
2024-01-04 23:44:18 +0800

    Fix ( Home Page page) : Modified Home Page.
    details:
    
    change TopNavHP to TopNavInside Fixed.
    
    Please refer to : Nazrin
    from branch : Main's Branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,31 +9,6 @@
 	<div class="container flex items-center justify-center h-full mx-auto" in:fade>
 		<div class="w-full h-full space-y-5">
 			<h1 class="text-center h1">Welcome</h1>
-
-			<div class="flex w-full h-full space-x-5">
-				<div class="w-1/2 p-2 bg-red-400 rounded-xl h-fix">
-					<div class="flex items-center justify-center">
-						<div>(Image)</div>
-					</div>
-					<div>name</div>
-				</div>
-				<div class="w-1/2 p-2 bg-green-400 rounded-xl h-fix">
-					<div>about Me</div>
-					<div>Objective</div>
-					<div>Objective</div>
-				</div>
-			</div>
-			<div class="p-2 bg-blue-400 rounded-xl">
-				<div>school</div>
-				<div>school</div>
-				<div>school</div>
-				<div>school</div>
-				<div>school</div>
-				<div>school</div>
-			</div>
-			<div class="w-full p-2 bg-yellow-400 rounded-xl">
-				<div>skills</div>
-			</div>
 		</div>
 	</div>
 </div>
--- src/routes/AboutMe/+page.svelte
+++ src/routes/AboutMe/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import TopNavHp from '$lib/component/topnav/TopNavHP.svelte';
+	import TopNavInside from '$lib/component/topnav/TopNavInside.svelte';
 	import { fade } from 'svelte/transition';
 </script>
 
 <div class="p-2">
-	<TopNavHp></TopNavHp>
+	<TopNavInside></TopNavInside>
 	<div class="container flex items-center justify-center h-full mx-auto" in:fade>
 		<div class="w-full h-full space-y-5">
 			<h1 class="text-center h1">Hi (Name)</h1>
